dcd2f18e4bf7e91bc9714a7703883467abd8d593
Colin Kennedy <colinvfx@gmail.com>
2021-02-13 20:57:31 -0800

    Added the basic dot-finder related files

---
+++ ftplugin/python/dot_finder.vim
@@ -0,0 +1,155 @@
+" A module which traverses Python files and code to get a dot-separated path to the user's cursor.
+"
+" See Also:
+"     :func:`GetCurrentDotPath`
+"
+
+
+" Get the Python module dot path to a given Python file.
+"
+" It's assumed that `reference` actually is within a Rez package.
+"
+" Args:
+"     reference (str):
+"         The absolute path to a file or folder on-disk. It must be
+"         within a Rez package.
+"
+" Returns:
+"     str:
+"         The found module dot path, if any. e.g. "tests.test_foo".
+"         This path does not contain Python class / function / method
+"         information. Just the module names.
+"
+function! s:get_rez_dot_path(reference)
+    let l:previous = ""
+    let l:current = fnamemodify(a:reference, ":p:r")
+    let l:names = []
+    let l:package_path = ""
+
+    while l:previous != l:current
+        call add(l:names, fnamemodify(l:current, ":t"))
+
+        let l:package_path = s:get_package_path(l:current)
+
+        if filereadable(l:package_path)
+            break
+        endif
+
+        let l:previous = l:current
+        let l:current = fnamemodify(l:current, ":h")
+    endwhile
+
+    if l:package_path == ""
+        echoerr "No package root could be found."
+
+        return ""
+    endif
+
+    let l:names = reverse(l:names)
+    let l:names = s:resolve_module_names(l:names, l:package_path)
+
+    return join(l:names, ".")
+endfunction
+
+
+" Find the Rez package within `directory`, if it exists.
+"
+" Args:
+"     directory (str): A folder, on-disk, where there may be a Rez package file.
+"
+" Returns:
+"     str: The found package path, if any.
+"
+function! s:get_package_path(directory)
+    for l:possibility in [
+    \ a:directory . "/package.py",
+    \ a:directory . "/package.yaml",
+    \ a:directory . "/package.yml",
+    \ ]
+        if filereadable(l:possibility)
+            return l:possibility
+        endif
+    endfor
+
+    return ""
+endfunction
+
+
+" Get the Python module names which most closely resemble their import path.
+"
+" Args:
+"     names (list[str]): Each found Python folder. They usually either assume
+"     that the Python files are within a Rez package, within a folder called
+"     "python" or assume that the Python files are defined, starting at the
+"     root of the Python package. e.g. ["rez_utilities", "python", "rez_utilities", "blah.py"]
+"     or ["rez_utilities", "rez_utilities", "blah.py"]
+"
+" Returns:
+"     list[str]: The folders without the first 1 or 2 folder names.
+"
+function! s:resolve_module_names(names, package_path)
+    if a:names[1] == "python"
+        return a:names[2:]
+    endif
+
+    return a:names[1:]
+endfunction
+
+
+" Find the Python dot path to the user's current row / column position.
+"
+" Args:
+"     buffer (container[str]):
+"         Every Python source code line which will be parsed for a dot path.
+"     row (int):
+"         A 0-based index value for the user's cursor position.
+"     column (int, optional):
+"         A 0-based index value for the user's cursor offset.
+"
+" Returns:
+"     str: The found dot path "Class.get_bar".
+"
+function! s:_get_inner_dot_path(buffer, row, column)
+pythonx << EOF
+import dot_finder
+import vim
+
+path = dot_finder.get_dot_path(
+    int(vim.eval("a:row")),
+    vim.eval("a:buffer"),
+    int(vim.eval("a:column")),
+)
+vim.command('let l:result = "{path}"'.format(path=path))
+EOF
+
+return l:result
+endfunction
+
+
+" str: Get the user's current Python dot-import location. e.g. "tests.test_foo.Class.get_bar"
+function! GetCurrentDotPath()
+    let l:current_path = expand("%:p")
+    let l:current_buffer = getline(1, '$')
+    let l:cursor = getpos(".")
+    let l:row = l:cursor[1]
+    let l:column = l:cursor[2]
+
+    let l:root = s:get_rez_dot_path(l:current_path)
+    let l:tail = s:_get_inner_dot_path(l:current_buffer, l:row - 1, l:column)
+
+    if l:tail == ""
+        return l:root
+    endif
+
+    return l:root . "." . l:tail
+endfunction
+
+
+" How to send the dot path to the top terminal buffer
+"
+" `call SendTextToTopTerminal("python -m unittest " . GetCurrentDotPath())`
+" `nnoremap <silent> <leader>x :call SendTextToTopTerminal("python -m unittest " . GetCurrentDotPath())<CR>`
+"
+command! -buffer -nargs=0 EchoCurrentDotPath :echo GetCurrentDotPath()
+
+autocmd FileType python nnoremap <leader>pd :EchoCurrentDotPath<CR>
---
+++ pythonx/dot_finder.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""A module which traverses Python code and gets a dot-separated path to Python objects.
+
+See Also:
+    :func:`get_dot_path`.
+
+"""
+
+import ast
+
+try:
+    _CANDIDATES = (ast.AsyncFunctionDef, ast.ClassDef, ast.FunctionDef)  # Python 3
+except AttributeError:
+    _CANDIDATES = (ast.FunctionDef, ast.ClassDef)  # Python 2
+
+
+def _compute_interval(node):
+    """Get the start and end positions of some AST node.
+
+    Args:
+        node (:class:`ast.AST`): Some Python node to query from.
+
+    Returns:
+        tuple[int, int]:
+            Two 0-based positions, indicating the row that the source
+            code of `node` starts on and ends on.
+
+    """
+    minimum = node.lineno
+    maximum = node.lineno
+
+    for node_ in ast.walk(node):
+        if hasattr(node_, "lineno"):
+            minimum = min(minimum, node_.lineno)
+            maximum = max(maximum, node_.lineno)
+
+    return (minimum, maximum + 1)
+
+
+def _get_inner_dot_path(node):
+    """Get the Python dot-path for an AST node.
+
+    This function assumes that the AST node has been optionally extended
+    to have a parent node.
+
+    See Also:
+        :func:`_text_to_tree`.
+
+    Args:
+        node (:class:`ast.AST`):
+            The AST node which we assume is a Python class / function /
+            method definition.
+
+    Returns:
+        str: The found Python dot-separated path. e.g. "tests.test_foo.Class.get_blah".
+
+    """
+    parent = node
+    name = []
+
+    while hasattr(parent, "parent"):
+        name.append(parent.name)
+
+        parent = parent.parent
+
+    return ".".join(reversed(name))
+
+
+def _get_node_or_parent(row, tree, column, fallback):
+    """Check if `fallback` should be returned or a parent node in the `tree`.
+
+    Args:
+        row (int):
+            A 0-based index value for the user's cursor position.
+        tree (dict[int, :class:`ast.AST`]):
+            A parsed tree of Python class / function / method definitions.
+        column (int):
+            A 0-based index value for the user's cursor offset.
+        fallback (:class:`ast.AST`):
+            Some node to return, assuming that the user did not mean to
+            return a node in `tree`.
+
+    Returns:
+        :class:`ast.AST`: Either `fallback` or another node within `tree`.
+
+    """
+    test_row = row
+    indentation = 4  # Assuming PEP-8
+
+    while test_row:
+        test_row -= 1
+        test_node = tree[test_row]
+
+        if not hasattr(test_node, "col_offset"):
+            continue
+
+        if column - indentation >= test_node.col_offset:
+            return test_node
+
+        return fallback
+
+
+def _text_to_tree(graph, line_count):
+    """Get every class / function / method from a AST Python module.
+
+    Args:
+        graph (:class:`ast.Module`): Some Python module to parse.
+        line_count (int): The total lines in the file.
+
+    Raises:
+        ValueError: If `line_count` is less than 0.
+
+    Returns:
+        dict[int, :class:`ast.AST`]:
+            Each node found on each line. If the line does not define a
+            class, function, or method, `graph` is used instead.
+
+    """
+    if line_count < 0:
+        raise ValueError(
+            'Line count "{line_count}" cannot be less than zero.'.format(
+                line_count=line_count
+            )
+        )
+
+    tree = dict()
+
+    for node in ast.walk(graph):
+        # 1. Add parent data to each node so that it can be queried, later
+        for child in ast.iter_child_nodes(node):
+            child.parent = node
+
+        # 2. Add each node, per-line, to our tree
+        if isinstance(node, ast.Module):
+            tree.update(dict.fromkeys(range(line_count), node))
+        elif isinstance(node, _CANDIDATES):
+            start, end = _compute_interval(node)
+            tree.update(dict.fromkeys(range(start, end), node))
+
+    return tree
+
+
+def get_dot_path(row, lines, column=0):
+    """Get the Python dot-path for the user's requested cursor position.
+
+    Args:
+        row (int):
+            A 0-based index value for the user's cursor position.
+        lines (container[str]):
+            Every Python source code line which will be parsed for a dot path.
+        column (int, optional):
+            A 0-based index value for the user's cursor offset.
+
+    Raises:
+        ValueError: If `row` is to big and exceeds the length of `lines`.
+
+    Returns:
+        str: The found Python dot-separated path. e.g. "tests.test_foo.Class.get_blah".
+
+    """
+    size = len(lines)
+
+    if row > size:
+        raise ValueError(
+            'Row "{row}" cannot be greater that line length, "{size}".'.format(
+                row=row, size=size
+            )
+        )
+
+    graph = ast.parse("\n".join(lines))
+    tree = _text_to_tree(graph, size)
+    node = tree[row]
+
+    if not column:
+        return _get_inner_dot_path(node)
+
+    node = _get_node_or_parent(row, tree, column, node)
+
+    return _get_inner_dot_path(node)
